9fb49148a79cca52013eac5ac798d8758fc9db37
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-03-16 14:03:35 -1000

    Initial Commit

---
+++ EdutainmentTestApp/EdutainmentTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentTestAppApp.swift
+//  EdutainmentTestApp
+//
+//  Created by Arseniy Oksenoyt on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
